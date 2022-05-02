4a26a477a5c3cfb4494a1db518fe6b1a907b5f28
Amauri Clementino <amaurisantospro@gmail.com>
2022-05-02 19:48:35 -0300

    Bank file configuration

---
+++ bank.py
@@ -0,0 +1,45 @@
+from typing import List
+from time import sleep
+
+from models.client import Client
+from models.account import Account
+
+
+accounts: List[Account] = []
+
+
+def main() -> None:
+    menu()
+
+
+def menu() -> None:
+    pass
+
+
+def create_account() -> None:
+    pass
+
+
+def make_withdraw() -> None:
+    pass
+
+
+def make_deposit() -> None:
+    pass
+
+
+def make_transfer() -> None:
+    pass
+
+
+def list_accounts() -> None:
+    pass
+
+
+def get_account_by_account_number(account_number: int) -> Account:
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
